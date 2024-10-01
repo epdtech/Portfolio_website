39d0cca656fe0e82a2ab8c859245528bd9f7ccc0
epdtech <118341264+epdtech@users.noreply.github.com>
2024-10-01 15:53:24 +0200

    Update portfolio_website2.py

--- portfolio_website2.py
+++ portfolio_website2.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import google.generativeai as genai
 
+st.set_page_config(
+   page_title="Ex-stream-ly Cool App",
+   page_icon="🧊",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
 api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
@@ -9,12 +16,7 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 
 
 
-st.set_page_config(
-   page_title="Ex-stream-ly Cool App",
-   page_icon="🧊",
-   layout="wide",
-   initial_sidebar_state="expanded",
-)
+
 
 col1, col2 = st.columns(2)
 
